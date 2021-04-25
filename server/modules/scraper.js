9919aed67e4a26c5bd40f608714782455d588e3e
Luke Haas <lukehaas7@gmail.com>
2021-04-25 19:29:12 +0100

    set no sandbox

--- server/modules/scraper.js
+++ server/modules/scraper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 const getImage = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto(process.env.HOST, {
     waitUntil: 'networkidle2',
