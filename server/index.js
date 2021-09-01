82a82a840bb6544457d2e34a064e679e42eaeada
Luke Haas <lukehaas7@gmail.com>
2021-09-01 17:09:10 +0100

    set cache

--- server/index.js
+++ server/index.js
@@ -21,6 +21,10 @@ const nodeEnv = process.env.NODE_ENV;
 
 server.applyMiddleware({ app });
 
+app.use(function (req, res, next) {
+  res.set('Cache-control', 'max-age=300');
+});
+
 if (nodeEnv === 'development') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
